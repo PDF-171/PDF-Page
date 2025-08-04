acd743fa36014d85d40b843f3df1dbc59185ec92
PDF-171 <khris.drakester171@gmail.com>
2025-08-04 15:18:34 +0800

    Update turningpage.js

--- turningpage.js
+++ turningpage.js
@@ -1,30 +1,57 @@
 <!DOCTYPE html>
 <html>
 <head>
-    <title>Turn.js Example</title>
-    <link rel="stylesheet" type="text/css" href="css/basic.css">
-    <script type="text/javascript" src="js/jquery.min.js"></script>
-    <script type="text/javascript" src="js/turn.min.js"></script>
+  <title>Turn.js Example</title>
+  <link rel="stylesheet" type="text/css" href="css/basic.css">
+  <script type="text/javascript" src="js/jquery.min.js"></script>
+  <script type="text/javascript" src="js/turn.min.js"></script>
+  <style>
+    body {
+      margin: 0;
+      background: #111;
+      display: flex;
+      justify-content: center;
+      align-items: center;
+      height: 100vh;
+    }
+
+    #book {
+      width: 1200px; /* 600 x 2 pages */
+      height: 850px;
+    }
+
+    .page {
+      width: 600px;
+      height: 850px;
+      overflow: hidden;
+    }
+
+    .page img {
+      width: 100%;
+      height: 100%;
+      object-fit: contain;
+    }
+  </style>
 </head>
 <body>
 
 <div id="book">
-    <div class="page">Page 1 Content</div>
-    <div class="page">Page 2 Content</div>
-    <div class="page">Page 3 Content</div>
-    <div class="page">Page 4 Content</div>
+  <div class="page"><img src="apollo-flyer-page1.jpg" alt="Page 1"></div>
+  <div class="page"><img src="apollo-flyer-page2.jpg" alt="Page 2"></div>
+  <div class="page">Page 3 Content</div>
+  <div class="page">Page 4 Content</div>
 </div>
 
-<script type="text/javascript">
-    $(window).ready(function() {
-        $('#book').turn({
-            width: 800,
-            height: 600,
-            autoCenter: true,
-            gradients: true,
-            elevation: 50
-        });
+<script>
+  $(window).ready(function() {
+    $('#book').turn({
+      width: 1200,      // 600px per page × 2
+      height: 850,      // fits most screens, matches your aspect ratio
+      autoCenter: true,
+      gradients: true,
+      elevation: 50
     });
+  });
 </script>
 
 </body>
